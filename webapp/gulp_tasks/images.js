94b9c547ef006955a2d20487942f90ce73b41758
unknown <pbouadjie@W-PBOUADJIE.eu.accor.net>
2015-10-23 18:02:18 +0200

    petite avancement

--- webapp/gulp_tasks/images.js
+++ webapp/gulp_tasks/images.js
@@ -1,9 +1,9 @@
-/**
- * Created by Bebz on 23/10/2015.
- */
-
 var gulp = require('gulp');
+var buildConfig = require('../../config/config.json')['build'];
+var component = process.env.INIT_CWD.split((__dirname.split('gulp_tasks')[0]))[1];
+var distFolder = __dirname + '/../../dist/' + component;
 
 gulp.task('images', function(){
-    return;
+    return gulp.src(__dirname + '/../' + component + '/' + buildConfig.src + '/**/*.jpeg', {base: buildConfig.src})
+        .pipe(gulp.dest(distFolder));
 });
--- webapp/shrtlstd/app/components/home/homeCtrl.js
+++ webapp/shrtlstd/app/components/home/homeCtrl.js
@@ -3,9 +3,31 @@
  */
 
 angular.module('homeModule', [])
-    .controller('homeCtrl', [function(){
+    .controller('homeCtrl', ['$http', function($http){
         'use strict';
         var self = this;
 
         self.message = 'toto';
+
+        self.identification = {
+        	connection : "Se connecter",
+        	creation : "Créer un compte"
+        }
+
+        self.lastcompt = [];
+
+        function displayLatestCompet() {
+        	$http({method: "GET", url: "http://54.201.37.156:8080/short-listed/rest/projets/inProgress"}).
+		        then(function(response) {
+		          	self.status = response.status;
+		          	self.data = response.data;
+		          	console.log(self.data);
+		        }, function(response) {
+		          	self.data = response.data || "Request failed";
+		         	self.status = response.status;
+		         	console.log(self.data);
+		    });
+        }
+
+        displayLatestCompet();
     }]);

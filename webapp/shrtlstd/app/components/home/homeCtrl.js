d1f88de2303f15fa51d086d8ab9e94e4df8a4810
unknown <pbouadjie@W-PBOUADJIE.eu.accor.net>
2015-10-26 14:35:34 +0100

    Ajout de la connexion

--- webapp/shrtlstd/app/components/home/homeCtrl.js
+++ webapp/shrtlstd/app/components/home/homeCtrl.js
@@ -14,20 +14,65 @@ angular.module('homeModule', [])
         	creation : 'Créer un compte'
         };
 
-        self.lastcompt = [];
+        self.lastcompt = [{
+			projetId: "RYhPKITz"
+			nom: "projet1"
+			fileName: null
+			nbEquipeMax: 2
+			nbUserByEquipe: 3
+			dateDepot: "2015-10-26"
+			dateFin: "2015-12-25"
+			description: "desc 1"
+			recompense: "poste"
+			user: {
+				login: null
+				userType: null
+				roles: null
+				pseudo: "pro"
+				nom: "pro"
+				prenom: ""
+				avatar: null
+				situationPro: null
+				metier: null
+				entreprise: ""
+				description: null
+				sitePerso: null
+				sitePro: ""
+				}
+			}];
 
-        function displayLatestCompet() {
-        	$http({method: 'GET', url: 'http://54.201.37.156:8080/short-listed/rest/projets/inProgress'}).
+		auntheticate();
+
+		/*Permet de se connecter avec un email et un mdp*/
+        function auntheticate() {
+        	$http({method: 'POST', url: 'http://52.25.41.26:8080/short-listed/rest/authenticate?login=root1234@gmail.com&password=Root1234$'}).
 		        then(function(response) {
 		          	self.status = response.status;
 		          	self.data = response.data;
-		          	console.log(self.data);
+		          	console.log("self.data1 = ", self.data);
+
+		          	//displayLatestCompet();
+
 		        }, function(response) {
 		          	self.data = response.data || 'Request failed';
 		         	self.status = response.status;
-		         	console.log(self.data);
+		         	console.log("self.data2 = ", self.data);
 		    });
-        }
+        };
 
-        displayLatestCompet();
+        /*function displayLatestCompet() {
+        	$http({method: 'GET', url: 'http://52.25.41.26:8080/short-listed/rest/projets/inProgress'}).
+		        then(function(response) {
+		          	self.status = response.status;
+		          	self.data = response.data;
+		          	console.log("self.data1 = ", self.data);
+		        }, function(response) {
+		          	self.data = response.data || 'Request failed';
+		         	self.status = response.status;
+		         	console.log("self.data2 = ", self.data);
+		    });
+        }*/
+        
+        
+
     }]);

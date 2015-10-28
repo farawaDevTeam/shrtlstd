702663288dec2cde5631e0d409eeb5b2c075abaf
Bebz_229 <jtchitchi@gmail.com>
2015-10-28 18:11:44 +0100

    creating connexion template

--- webapp/shrtlstd/app/components/connexion/connexionCtrl.js
+++ webapp/shrtlstd/app/components/connexion/connexionCtrl.js
@@ -1,8 +1,12 @@
 angular.module('connexionModule', [])
-	.controller('connexionCtrl', function(){
+	.controller('connexionCtrl', function(langService){
 		'use strict';
 		
 		var self = this;
+		
+		langService.getLabels(function(labels){
+			self.labels = labels;
+		});
 		
 		self.credentials = {};
 	});

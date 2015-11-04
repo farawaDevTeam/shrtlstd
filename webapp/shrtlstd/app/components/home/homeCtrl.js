a7df4a4d13813ad07f2eae9413362c5b70fd60cb
Bebz_229 <jtchitchi@gmail.com>
2015-11-04 16:43:23 +0100

    remove useless connexion in home controller

--- webapp/shrtlstd/app/components/home/homeCtrl.js
+++ webapp/shrtlstd/app/components/home/homeCtrl.js
@@ -3,32 +3,32 @@
  */
 
 angular.module('homeModule', [])
-    .controller('homeCtrl', function($http, langService){
+    .controller('homeCtrl', function ($http, langService) {
         'use strict';
         var self = this;
 
-		langService.getLabels(function(labels){
+		langService.getLabels(function (labels) {
 			self.labels = labels;
 		});
 
         self.colSizeLgMd = {
-        	0: 6,
-        	1: 4,
-        	2: 2,
-        	3: 3,
-        	4: 6,
-        	5: 3
+			0: 6,
+			1: 4,
+			2: 2,
+			3: 3,
+			4: 6,
+			5: 3
         };
 
-        self.getMosaic = function(index){
-        	var divClass = {};
-        	divClass['col-lg-' + self.colSizeLgMd[index]] = true;
-        	divClass['col-md-' + self.colSizeLgMd[index]] = true;
-        	return divClass;
+        self.getMosaic = function (index) {
+			var divClass = {};
+			divClass['col-lg-' + self.colSizeLgMd[index]] = true;
+			divClass['col-md-' + self.colSizeLgMd[index]] = true;
+			return divClass;
         };
 
         self.lastcompets = [{
-        	img : '1.jpg',
+			img: '1.jpg',
 			projetId: 'RYhPKITz',
 			nom: 'Refonde du logo',
 			fileName: null,
@@ -53,172 +53,141 @@ angular.module('homeModule', [])
 				sitePerso: null,
 				sitePro: ''
 			}
-		}, 
-		{
-			img : '2.jpg',
-			projetId: 'RYhPKITz',
-			nom: 'Campagne de pub Galaxy S6 Edge',
-			fileName: null,
-			nbEquipeMax: 2,
-			nbUserByEquipe: 3,
-			dateDepot: '2015-10-26',
-			dateFin: '2015-12-25',
-			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-			recompense: 'poste',
-			user: {
-				login: null,
-				userType: null,
-				roles: null,
-				pseudo: 'pro',
-				nom: 'pro',
-				prenom: '',
-				avatar: null,
-				situationPro: null,
-				metier: null,
-				entreprise: 'Samsung',
-				description: null,
-				sitePerso: null,
-				sitePro: ''
-			}
-		}, 
-		{
-			img : '3.jpg',
-			projetId: 'RYhPKITz',
-			nom: 'Affiche publicitaire',
-			fileName: null,
-			nbEquipeMax: 2,
-			nbUserByEquipe: 3,
-			dateDepot: '2015-10-26',
-			dateFin: '2015-12-25',
-			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-			recompense: 'poste',
-			user: {
-				login: null,
-				userType: null,
-				roles: null,
-				pseudo: 'pro',
-				nom: 'pro',
-				prenom: '',
-				avatar: null,
-				situationPro: null,
-				metier: null,
-				entreprise: 'Lidl',
-				description: null,
-				sitePerso: null,
-				sitePro: ''
-			}
-		}, 
-		{
-			img : '4.jpg',
-			projetId: 'RYhPKITz',
-			nom: 'Stratégie de communication',
-			fileName: null,
-			nbEquipeMax: 2,
-			nbUserByEquipe: 3,
-			dateDepot: '2015-10-26',
-			dateFin: '2015-12-25',
-			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-			recompense: 'poste',
-			user: {
-				login: null,
-				userType: null,
-				roles: null,
-				pseudo: 'pro',
-				nom: 'pro',
-				prenom: '',
-				avatar: null,
-				situationPro: null,
-				metier: null,
-				entreprise: 'AccorHotels',
-				description: null,
-				sitePerso: null,
-				sitePro: ''
-			}
-		}, 
-		{
-			img : '1.jpg',
-			projetId: 'RYhPKITz',
-			nom: 'Vidéo',
-			fileName: null,
-			nbEquipeMax: 2,
-			nbUserByEquipe: 3,
-			dateDepot: '2015-10-26',
-			dateFin: '2015-12-25',
-			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-			recompense: 'poste',
-			user: {
-				login: null,
-				userType: null,
-				roles: null,
-				pseudo: 'pro',
-				nom: 'pro',
-				prenom: '',
-				avatar: null,
-				situationPro: null,
-				metier: null,
-				entreprise: 'SFR',
-				description: null,
-				sitePerso: null,
-				sitePro: ''
-			}
-		}, 
-		{
-			img : '5.jpg',
-			projetId: 'RYhPKITz',
-			nom: 'Powerpoint',
-			fileName: null,
-			nbEquipeMax: 2,
-			nbUserByEquipe: 3,
-			dateDepot: '2015-10-26',
-			dateFin: '2015-12-25',
-			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-			recompense: 'poste',
-			user: {
-				login: null,
-				userType: null,
-				roles: null,
-				pseudo: 'pro',
-				nom: 'pro',
-				prenom: '',
-				avatar: null,
-				situationPro: null,
-				metier: null,
-				entreprise: 'H&M',
-				description: null,
-				sitePerso: null,
-				sitePro: ''
-			}
-		}];
+		},
+			{
+				img: '2.jpg',
+				projetId: 'RYhPKITz',
+				nom: 'Campagne de pub Galaxy S6 Edge',
+				fileName: null,
+				nbEquipeMax: 2,
+				nbUserByEquipe: 3,
+				dateDepot: '2015-10-26',
+				dateFin: '2015-12-25',
+				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+				recompense: 'poste',
+				user: {
+					login: null,
+					userType: null,
+					roles: null,
+					pseudo: 'pro',
+					nom: 'pro',
+					prenom: '',
+					avatar: null,
+					situationPro: null,
+					metier: null,
+					entreprise: 'Samsung',
+					description: null,
+					sitePerso: null,
+					sitePro: ''
+				}
+			},
+			{
+				img: '3.jpg',
+				projetId: 'RYhPKITz',
+				nom: 'Affiche publicitaire',
+				fileName: null,
+				nbEquipeMax: 2,
+				nbUserByEquipe: 3,
+				dateDepot: '2015-10-26',
+				dateFin: '2015-12-25',
+				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+				recompense: 'poste',
+				user: {
+					login: null,
+					userType: null,
+					roles: null,
+					pseudo: 'pro',
+					nom: 'pro',
+					prenom: '',
+					avatar: null,
+					situationPro: null,
+					metier: null,
+					entreprise: 'Lidl',
+					description: null,
+					sitePerso: null,
+					sitePro: ''
+				}
+			},
+			{
+				img: '4.jpg',
+				projetId: 'RYhPKITz',
+				nom: 'Stratégie de communication',
+				fileName: null,
+				nbEquipeMax: 2,
+				nbUserByEquipe: 3,
+				dateDepot: '2015-10-26',
+				dateFin: '2015-12-25',
+				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+				recompense: 'poste',
+				user: {
+					login: null,
+					userType: null,
+					roles: null,
+					pseudo: 'pro',
+					nom: 'pro',
+					prenom: '',
+					avatar: null,
+					situationPro: null,
+					metier: null,
+					entreprise: 'AccorHotels',
+					description: null,
+					sitePerso: null,
+					sitePro: ''
+				}
+			},
+			{
+				img: '1.jpg',
+				projetId: 'RYhPKITz',
+				nom: 'Vidéo',
+				fileName: null,
+				nbEquipeMax: 2,
+				nbUserByEquipe: 3,
+				dateDepot: '2015-10-26',
+				dateFin: '2015-12-25',
+				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+				recompense: 'poste',
+				user: {
+					login: null,
+					userType: null,
+					roles: null,
+					pseudo: 'pro',
+					nom: 'pro',
+					prenom: '',
+					avatar: null,
+					situationPro: null,
+					metier: null,
+					entreprise: 'SFR',
+					description: null,
+					sitePerso: null,
+					sitePro: ''
+				}
+			},
+			{
+				img: '5.jpg',
+				projetId: 'RYhPKITz',
+				nom: 'Powerpoint',
+				fileName: null,
+				nbEquipeMax: 2,
+				nbUserByEquipe: 3,
+				dateDepot: '2015-10-26',
+				dateFin: '2015-12-25',
+				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+				recompense: 'poste',
+				user: {
+					login: null,
+					userType: null,
+					roles: null,
+					pseudo: 'pro',
+					nom: 'pro',
+					prenom: '',
+					avatar: null,
+					situationPro: null,
+					metier: null,
+					entreprise: 'H&M',
+					description: null,
+					sitePerso: null,
+					sitePro: ''
+				}
+			}];
 
-		auntheticate();
-
-		/*Permet de se connecter avec un email et un mdp*/
-        function auntheticate() {
-        	$http({method: 'POST', url: 'http://52.25.41.26:8080/short-listed/rest/authenticate?login=root1234@gmail.com&password=Root1234$'}).
-		        then(function(response) {
-		          	self.status = response.status;
-		          	self.data = response.data;
-		          	console.log('self.data1 = ', self.data);
-
-		          	//displayLatestCompet();
-
-		        }, function(response) {
-		          	self.data = response.data || 'Request failed';
-		         	self.status = response.status;
-		         	console.log('self.data2 = ', self.data);
-		    });
-        }
-
-        /*function displayLatestCompet() {
-        	$http({method: 'GET', url: 'http://52.25.41.26:8080/short-listed/rest/projets/inProgress'}).
-		        then(function(response) {
-		          	self.status = response.status;
-		          	self.data = response.data;
-		          	console.log('self.data1 = ', self.data);
-		        }, function(response) {
-		          	self.data = response.data || 'Request failed';
-		         	self.status = response.status;
-		         	console.log('self.data2 = ', self.data);
-		    });
-        }*/
     });

fa1efd3b1758ffa806984aaf48dd8fd62322057c
Bebz_229 <jtchitchi@gmail.com>
2015-11-30 15:40:45 +0100

    start creation of facebook connection

--- webapp/shrtlstd/app/components/connexion/connexionService.js
+++ webapp/shrtlstd/app/components/connexion/connexionService.js
@@ -1,24 +1,34 @@
 angular.module('connexionModule')
-	.factory('connexionService', function(appConfig, $http){
+	.factory('connexionService', function (appConfig, $http, $window) {
 		'use strict';
-		
+
+		$window.fbAsyncInit = function () {
+			$window.FB.init({
+				appId: '{your-app-id}',
+				cookie: true,  // enable cookies to allow the server to access 
+				// the session
+				xfbml: true,  // parse social plugins on this page
+				version: 'v2.2' // use version 2.2
+			});
+		};
+
 		var connexionService = {};
-		
-		connexionService.connect = function(credentials){
-			return $http.post(appConfig.api.url + '/authenticate', 
-				'login=' + credentials.login + '&password=' + credentials.password, 
+
+		connexionService.connect = function (credentials) {
+			return $http.post(appConfig.api.url + '/authenticate',
+				'login=' + credentials.login + '&password=' + credentials.password,
 				{
-					headers: {'content-type': 'application/x-www-form-urlencoded'}
+					headers: { 'content-type': 'application/x-www-form-urlencoded' }
 				});
 		};
-		
-		connexionService.getUser = function(){
+
+		connexionService.getUser = function () {
 			return connexionService.user;
 		};
-		
-		connexionService.setUser = function(user){
+
+		connexionService.setUser = function (user) {
 			connexionService.user = user;
 		};
-		
+
 		return connexionService;
 	});
